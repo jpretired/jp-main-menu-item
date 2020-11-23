712dd9563b962c9da829de560f382c51a4da158d
jpretired <josef.prause@gmail.com>
2020-11-23 21:16:59 +0100

    První komit

---
+++ module.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Toto je modul pro položku Příručka v hlavním menu.
+ * Zhotoveno podle JustCarmen jc-simple-menu-1, funkce konfigurace je zrušena.
+ * JustCarmen webtrees modules
+ * Copyright (C) 2009-2020 Carmen Pijpers-Knegt
+ *
+ * Based on webtrees: online genealogy
+ * Copyright (C) 2020 webtrees development team
+ *
+ * This file is part of JustCarmen webtrees modules
+ *
+ * JustCarmen webtrees modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * JustCarmen webtrees modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with JustCarmen webtrees modules. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace JpNamespace;
+
+use Fisharebest\Webtrees\Auth;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Menu;
+use Fisharebest\Webtrees\Tree;
+use Fisharebest\Webtrees\View;
+use Fisharebest\Webtrees\FlashMessages;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleMenuTrait;
+use Fisharebest\Webtrees\Module\ModuleCustomTrait;
+use Fisharebest\Webtrees\Module\ModuleMenuInterface;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+use Fisharebest\Webtrees\Http\RequestHandlers\ModulesMenusAction;
+
+/**
+ * Anonymous class - provide a custom menu option and page
+ */
+return new class extends AbstractModule implements ModuleCustomInterface, ModuleMenuInterface
+{
+    use ModuleCustomTrait;
+    use ModuleMenuTrait;
+
+    /**
+     * How should this module be identified in the control panel, etc.?
+     *
+     * @return string
+     */
+    public function title(): string
+    {
+        return 'Příručka webtrees';
+    }
+
+    /**
+     * A sentence describing what this module does.
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        /* I18N: Description of the “Simple Menu” module */
+        return I18N::translate('Easily add an extra mainmenu item and page to your webtrees website.');
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleAuthorName()
+     */
+    public function customModuleAuthorName(): string
+    {
+        return 'Josef Prause';
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleVersion()
+     *
+     * We use a system where the version number is equal to the latest version of webtrees
+     * Interim versions get an extra sub number
+     *
+     * The dev version is always one step above the latest stable version of this module
+     * The subsequent stable version depends on the version number of the latest stable version of webtrees
+     *
+     */
+    public function customModuleVersion(): string
+    {
+        return '1.0';
+    }
+
+    /**
+     * A URL that will provide the latest stable version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersionUrl(): string
+    {
+        return 'https://raw.githubusercontent.com/JustCarmen/webtrees-simple-menu/master/latest-version.txt';
+    }
+
+    /**
+     * Bootstrap the module
+     */
+    public function boot(): void
+    {
+        // Register a namespace for our views.
+        View::registerNamespace($this->name(), $this->resourcesFolder() . 'views/');
+    }
+
+     /**
+     * Where does this module store its resources
+     *
+     * @return string
+     */
+    public function resourcesFolder(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * The default position for this menu.  It can be changed in the control panel.
+     *
+     * @return int
+     */
+    public function defaultMenuOrder(): int
+    {
+        return 99;
+    }
+
+    /**
+     * A menu, to be added to the main application menu.
+     *
+     * @param Tree $tree
+     *
+     * @return Menu|null
+     */
+    public function getMenu(Tree $tree): ?Menu
+    {
+        if ($tree === null) {
+            return '';
+        }
+        $url = 'https://manu.jprodina.cz';
+        $menu_title = 'Příručka';
+
+        return new Menu($menu_title, e($url), 'jp-prirucka' /* . e(strtolower($menu_title))*/);
+    }
+};
